1daa891a7c8253f22fbe61bac8220846f5350bda
william <williamcrixi@gmail.com>
2018-05-21 16:53:47 -0300

    identação do sobre

--- sobre.php
+++ sobre.php
@@ -1,5 +1,9 @@
-    <?php session_start(); ?>
-    <?php require 'navbar.php'; ?>
+<?php
+    session_start();
+?>
+<?php
+    require 'navbar.php';
+?>
 <!--Equipe-->
     <section class="equipe grey lighten-3">
         <div class="container">
