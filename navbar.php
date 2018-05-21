34b9d2d73566ba465a96034c353234a12f48325f
vitor <vitorthomaz111@gmail.com>
2018-05-21 13:54:45 -0300

    imagem do usuario logado

--- navbar.php
+++ navbar.php
@@ -115,7 +115,7 @@
           <li><a href="depoimentos.php">Depoimentos</a></li>
 			
         <ul class="right hide-on-med-and-down" id="areaLogin">
-          <li><a href="javascript:;" onclick="login()">Cadastrar/Entrar<img class="avatar" src="img/<?php echo $row['img']; ?>"></a></li>
+          <li><a href="javascript:;" onclick="login()">Cadastrar/Entrar<img class="avatar" id="avatarnavbar" src="img/<?php echo $row['img']; ?>"></a></li>
         </ul>
 			
         </ul>
@@ -164,6 +164,6 @@
 ?>
 <script type="text/javascript">
   $("#areaLogin").html('<li><a class="dropdown-button" data-activates="dropdown3" href="javascript:;"><?php echo $nome; ?><img class="avatar" src="img/<?php echo $img; ?>"></a></li>');
-  $("#areaLoginMobile").html('<div class="user-view"><div class="background"><a href="javascript:;" onclick="login()"><img src="img/ground.png"></a></div><a href="javascript:;" onclick="login()"><img class="circle" src="img/<?php echo $img; ?>"></a><a href="javascript:;" onclick="login()"><span class="white-text name"><?php echo $nome; ?></span></a></div>');
+  $("#areaLoginMobile").html('<div class="user-view"><div class="background"><a href="javascript:;" onclick="login()"><img src="img/ground.png"></a></div><a href="javascript:;" onclick="login()"><img class="circle" id="avatarnavbarlogado" src="img/<?php echo $img; ?>"></a><a href="javascript:;" onclick="login()"><span class="white-text name"><?php echo $nome; ?></span></a></div>');
 </script>
 <?php  } ?>
