8bf0b4dcd328f68bcd55b100c0bf1c8775189b42
Sandro Mariano Silva Filho <sandrolalo@hotmail.com>
2018-06-20 15:14:48 -0300

    fotos_banco

--- findpet-dados.sql
+++ findpet-dados.sql
@@ -49,15 +49,15 @@ INSERT INTO `mapa` (`idmapa`, `idanimal`, `latitude`, `longitude`) VALUES
 
 
 INSERT INTO `usuario` (`idusuario`, `nome`, `email`, `img`, `telefone`, `senha`) VALUES
-(1, 'Sandro Mariano', 'sandrolalo@hotmail.com', 'profile.jpg', '12997261834', 'a5d124e34bcdc8f005b7d06ad05d5944e499a50e'),
-(2, 'Janayna Thomaz ', 'janayna.stefanini@gmail.com ', 'profile.jpg', '(12) 98208-6181', 'cb94cab4d2c696f73fd42be369ac28fb1faf6cf7'),
-(3, 'Gabriel Crisostomo Soares de Farias', 'gabrielcrisostomo2001@outlook.com', 'profile.jpg', '(12) 98131-9911', 'ded43e5a454fe1bd2c09bfd499db518348abdb75'),
-(4, 'Caroline Rocha', 'carolinerocha034@gmail.com', 'profile.jpg', '(12) 99754-4206', '67cbf9b0aa143234fb538a36f234e1a30a5555f6'),
+(1, 'Sandro Mariano', 'sandrolalo@hotmail.com', 'profile-1.JPG', '12997261834', 'a5d124e34bcdc8f005b7d06ad05d5944e499a50e'),
+(2, 'Janayna Thomaz ', 'janayna.stefanini@gmail.com ', 'profile-2.jpg', '(12) 98208-6181', 'cb94cab4d2c696f73fd42be369ac28fb1faf6cf7'),
+(3, 'Gabriel Crisostomo S. de Farias', 'gabrielcrisostomo2001@outlook.com', 'profile-3.jpg', '(12) 98131-9911', 'ded43e5a454fe1bd2c09bfd499db518348abdb75'),
+(4, 'Caroline Rocha', 'carolinerocha034@gmail.com', 'profile-4.jpg', '(12) 99754-4206', '67cbf9b0aa143234fb538a36f234e1a30a5555f6'),
 (5, 'Bruno Barbosa', 'barbosabruno44@gmail.com', 'profile.jpg', '', '15697c06fc36e24fdb5e82e9a15fba63d8416b1d'),
 (6, 'mariozan', 'mariozanjunior15@gmail.com', 'profile.jpg', '(12) 98213-5453', '12126eefdb32e502cce66879e8280a4d83418270'),
 (7, 'william', 'williamcrixi@gmail.com', 'profile.jpg', '(12) 98134-0420', '9048ead9080d9b27d6b2b6ed363cbf8cce795f7f'),
 (8, 'Mario', 'mariozanjunior15@gmail.com', 'profile.jpg', '(12) 98213-5453', '8cb2237d0679ca88db6464eac60da96345513964'),
-(9, 'Mayara Barbosa', 'mbarbosam@gmail.com', 'profile.jpg', '(12) 98305-8603', 'ab222d26d933c6dcc99185dbd218ea61faabf8dd'),
-(10, 'Raphel Fernandes', 'raphafrc81@gmail.com', 'profile-10.jpeg', '(12) 982695053', 'a5d124e34bcdc8f005b7d06ad05d5944e499a50e');
+(9, 'Mayara Barbosa', 'mbarbosam@gmail.com', 'profile-9.jpg', '(12) 98305-8603', 'ab222d26d933c6dcc99185dbd218ea61faabf8dd'),
+(10, 'Raphel Fernandes', 'raphafrc81@gmail.com', 'profile-10.jpg', '(12) 982695053', 'a5d124e34bcdc8f005b7d06ad05d5944e499a50e');
 COMMIT;
 
