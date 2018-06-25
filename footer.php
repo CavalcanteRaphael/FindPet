c1f56b5f4b588f75a21a4d228ccfdb68c59d9b8f
MariozanJr <33577800+MariozanJr@users.noreply.github.com>
2018-06-25 15:48:02 -0300

    Pagina Ajude-nos, footer ajustes basicos

--- footer.php
+++ footer.php
@@ -3,15 +3,15 @@
         <div class="blocofooter row">
                 <div class="descrifooter col s12 m6 l3">
                     <h5 class="white-text">FindPet</h5>
-                    <p class="grey-text text-lighten-4">Findpet um site voltado para ajudar seu Pet</p>
+                    <p class="grey-text text-lighten-4">Findpet um site voltado <br/> para ajudar seu Pet</p>
                 </div>
                 <div class="col s12 m6 l3">
                     <h5>Serviços</h5>
                     <ul>
                         <li><a class="grey-text text-lighten-3"  href="adotar.php">Adotar</a></li>
                         <li><a class="grey-text text-lighten-3"  href="doacao.php">Doar</a></li>
-                        <li><a class="grey-text text-lighten-3"  href="mapa.php">Procurar</a></li>
                         <li><a class="grey-text text-lighten-3"  href="encontrado.php">Encontrados</a></li>  
+                        <li><a class="grey-text text-lighten-3"  href="mapa.php">Mapa dos Pets</a></li>
                     </ul>
                 </div>
                 <div class="col s12 m6 l3">
